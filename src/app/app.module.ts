4b1ce71bae7b22cbf9b24b391a129a06b962210b
EstebanM159 <estebanm91231@gmail.com>
2023-07-31 11:22:04 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SharedComponentsComponent } from './shared-components/shared-components.component';
+import { HeaderComponent } from './shared-components/header/header.component';
+import { FooterComponent } from './shared-components/footer/footer.component';
+import { CardComponent } from './shared-components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    SharedComponentsComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/personajes/personajes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListaPersonajesComponent } from './lista-personajes/lista-personajes.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListaPersonajesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PersonajesModule { }
