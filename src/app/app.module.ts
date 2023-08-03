cf7dc0b8ad857e5fb789beeaf2b17d89d0e0bedf
EstebanM159 <estebanm91231@gmail.com>
2023-08-03 09:49:33 -0300

    todo ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
@@ -21,7 +21,8 @@ import { PersonajesModule } from './personajes/personajes.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PersonajesModule
+    PersonajesModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared-components/header/header.component.ts
+++ src/app/shared-components/header/header.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { trigger,state,style,animate,transition } from '@angular/animations';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+ public activo:boolean=false;
+
+
+  abrir(){
+    this.activo=this.activo === true ? false : true;
+  }
 
 }
