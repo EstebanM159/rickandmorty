a71f8bae3d56b2bb385422eac375e3efb66ed940
EstebanM159 <estebanm91231@gmail.com>
2023-08-04 09:03:29 -0300

    Errores de nombres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ListaPersonajesComponent } from './personajes/lista-personajes/lista-personajes.component';
+import { ListaEpisodiosComponent } from './episodes-module/lista-episodios/lista-episodios.component';
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'lista-personajes',
+    pathMatch:'full'
+  },
+  {
+    path:'lista-personajes',
+    component:ListaPersonajesComponent
+  },
+  {
+    path: 'lista-episodios',
+    component:ListaEpisodiosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
