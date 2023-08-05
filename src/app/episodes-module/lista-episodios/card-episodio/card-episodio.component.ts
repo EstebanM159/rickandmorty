3509b90aae553b7181556df44f156389b166c162
EstebanM159 <estebanm91231@gmail.com>
2023-08-05 10:14:18 -0300

    Todo ok

--- src/app/episodes-module/lista-episodios/card-episodio/card-episodio.component.ts
+++ src/app/episodes-module/lista-episodios/card-episodio/card-episodio.component.ts
@@ -1,31 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ResultE } from 'src/app/models/episodes';
 import { EpisodiosService } from '../../episodios.service';
+import { Result } from 'src/app/models/characters';
 @Component({
   selector: 'app-card-episodio',
   templateUrl: './card-episodio.component.html',
   styleUrls: ['./card-episodio.component.css']
 })
-export class CardEpisodioComponent implements OnInit{
+export class CardEpisodioComponent {
   @Input() dataEpisodio?:ResultE ;
-  personajesEpisodio:string[]=[];
-  personajesDelEpisodio:string[]=[];
+  listaNombresPjs:string[]=[];
   constructor(private consulta:EpisodiosService){}
-  ngOnInit() {
-    this.cargarPersonajes();
-  }
-
   cargarPersonajes(){
-    if(this.dataEpisodio){
-      this.personajesEpisodio = this.dataEpisodio.characters
-    }
-    for (let i = 0; i < this.personajesEpisodio.length; i++) {
-      this.consulta.getCharacterByUrl(this.personajesEpisodio[i]).subscribe(data=>{
-        this.personajesDelEpisodio.push(data.name);
-      })
-
+    if(this.dataEpisodio ){
+      for (let i = 0; i < this.dataEpisodio.characters.length; i++) {
+        this.consulta.getCharacterByUrl(this.dataEpisodio.characters[i]).subscribe(data=>{
+          this.listaNombresPjs.push(data.name);
+        })
+      }
     }
   }
-
 
 }
